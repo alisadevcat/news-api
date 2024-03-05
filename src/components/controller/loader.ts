4ccb2c95d2d790f47d4ee0137d464ef360c7ba84
Alisa Raevskaya <bhumi.alisa@gmail.com>
2024-03-05 15:40:58 +0400

    feat: add callback type

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -38,7 +38,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method: string, endpoint: string, callback, options = {}) {
+    load(method: string, endpoint: string, callback: CallbackFunction<ApiResponse>, options = {}) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
